5db07b88db8ba7bca05835dfd36b496da82c7061
Karl Watson <karlmwatson@outlook.com>
2019-01-21 16:49:29 +0000

    feat: Functionality for creating (& clearing) the deck of cards

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
-/*
- * Create a list that holds all of your cards
- */
+const CARD_TYPES = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bomb', 'fa-bicycle'];
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -21,18 +12,26 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+// Builds one card
+function buildCard(cardType)  {
+  let deck = $('.deck');
+  deck.append(`<li class="card">
+      <i class="fa ${cardType}"></i>
+  </li>`);
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Fills the deck with all cards, after shuffling
+function buildAllCards()  {
+  let allCards = CARD_TYPES.concat(CARD_TYPES);
+  let shuffledCards = shuffle(allCards);
+  shuffledCards.forEach(buildCard);
+}
+
+// Empty Deck
+function clearAllCards(cardType)  {
+  let deck = $('.deck');
+  deck.empty();
+}
