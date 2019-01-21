2f7d88e59149b03f809d64f9a2f7f5f54abf9c6f
Karl Watson <karlmwatson@outlook.com>
2019-01-21 16:53:55 +0000

    feat: Functionality for the Timer
    Functions to:
    Test outputs in the console
    Store start time on first click of a cards
    Store end time on winning and calculate difference
    Convert above to time format:
      ss(2 decimal places)
      mm:ss(1 dp)
      hh:mm (1dp)
    Dependent on duration

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+// Global constants & variables:
 const CARD_TYPES = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bomb', 'fa-bicycle'];
+let isGameStarted = false;
+let timeLapsed = 0;
+let startTime = 0;
+let endTime = 0;
 
+
+//////////// Card functionality:
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -35,3 +42,49 @@ function clearAllCards(cardType)  {
   let deck = $('.deck');
   deck.empty();
 }
+
+
+//////////// Timer functionality:
+
+// uses the websites:
+// https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
+// https://www.w3schools.com/js/js_date_methods.asp
+// https://pawelgrzybek.com/rounding-and-truncating-numbers-in-javascript/
+
+// Show time: test output of different date / time variables: (to delete!!)
+function callTime() {
+  let d = new Date();
+  let a = d.getTime();
+  console.log('getTime():' + a);
+  console.log('Date.now():' + Date.now())
+}
+
+// Stores start time (in ms)
+function storeStartTime() {
+  startTime = Date.now();
+}
+
+// Calculates end time from end and start time values
+function calculateTime()  {
+  endTime = Date.now();
+  timeLapsed = (endTime - startTime);
+}
+
+// Convert time (stored in milliseconds) to a (human) readable format
+function convertTime(millisec) {
+   let seconds = (millisec / 1000).toFixed(2);
+   let minutes = (millisec / (1000 * 60)).toFixed(1);
+   let minutesRoundDown = Math.floor(minutes);
+   let hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+   let hoursRoundDown = Math.floor(hours);
+
+   if (seconds < 60) {
+       return seconds + " Seconds";
+   } else if (minutes < 60) {
+       return minutesRoundDown + " Minutes and " + (seconds - (60 * minutesRoundDown)).toFixed(1) + "seconds";
+   } else if (hours < 24) {
+       return hoursRoundDown + " Hours, " + Math.floor(minutes - (60 * hoursRoundDown)) + " minutes";
+   }
+}
+
+  
